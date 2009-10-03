47272f987827ff3f4cf31b20112215cb4ac857d7
Anders Eurenius <aes@nerdshack.com>
2009-10-03 23:24:07 +0200

    More mad hacking. Bookmarks implemented, but not tested.

--- dirt.py
+++ dirt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# {{{ head comment
 """
 dirt is an interactive curses user interface for changing directory in shells.
 
@@ -9,13 +9,17 @@ Put this in your .bashrc :
 
 function pcmd()
 {
-  local WD="$(pwd|sed "s@^${HOME}@\~@")"
-  echo "$DIRT" | grep "^${WD}$" - >/dev/null || DIRT="${DIRT}\n${WD}"
+  DIRT=$( echo ${DIRT}:${PWD} |\
+          tr : "\n" |\
+          sed "s@^${HOME}@\~@" |\
+          sort -u |\
+           tr "\n" : )
 }
 export DIRT=""
 PROMPT_COMMAND=pcmd
 alias d='eval $(dirt 2>&1 1>/dev/tty)'
 """
+# }}}
 
 import sys,os,curses as C,os.path
 import time
@@ -26,20 +30,59 @@ from os.path import isdir, normpath, expanduser, join as J
 # {{{ conveniences
 class sym: pass
 
-DIRT=sorted([ x for x in Env.get('DIRT','~/').split('\\n') if x ])
+class BookmarkFile(object):
+    """Abstraction for bookmark file.
+
+    Remember to make sure objects are explicitly destructed.
+    """
+    def __init__(self):
+        self.l = sorted([ l for l in file(expanduser('~/.dirt_bm')) ])
+        self.c = False
+    def __del__(self):
+        """Save if changed."""
+        self.save()
+    def save(self):
+        if not self.c: return
+        f = file(expanduser('~/.dirt_bm'), 'w')
+        f.write("\n".join(self.l))
+        f.close()
+    def append(self, d):
+        d = twiddle(d)
+        if d not in self.l: self.c, self.l = True, sorted(self.l+[d])
+    def remove(self, d):
+        d = twiddle(d)
+        if d in self.l: self.c, self.l = True, [x for x in self.l if x != d]
+    def __contains__(self, d): return twiddle(d) in self.l
+    def __iter__(self):        return self.l.__iter__()
+
+def read_homes():
+    f = file('/etc/passwd')
+    l = [ x.split(':') for x in f ]
+    f.close()
+    return dict([ (x[0],x[5]) for x in l
+                  if 999 < int(x[2]) < 1999 or int(x[2]) == 0])
+
+DIRT=sorted([ x for x in Env.get('DIRT','~/').split(':') if x ]
 OLDD=DIRT[:]
 HOME=Env.get('HOME')
+HOMES=read_homes()
+BOOK=BookmarkFile()
 
 def has_dir(p):
-    for n in [ x for x in listdir(p) if x[0] != '.' ]:
+    try:    l = listdir(p)
+    except: return False
+    for n in [ x for x in l if x[0] != '.' ]:
         if isdir(p+'/'+n):
             return True
 
 def twiddle(p, plus=False, H=HOME):
     p = untwid(p)
-    if plus and has_dir(p):  p = p+' +'
-    if p and p.find(H) == 0: p = '~'+p[len(H):]
-    if p:                    p = normpath(p)
+    if plus and has_dir(p):    p = p+' +'
+    if p and p.find(H) == 0:   p = '~'+p[len(H):]
+    else:
+        for u,d in HOMES.items():
+            if p.find(d) == 0: p = J('~'+u, p[len(d):])
+    if p:                      p = normpath(p)
     return p or './'
 
 def untwid(p=None):
@@ -88,24 +131,32 @@ class Menu(object): # {{{
                 if c: return c != Menu.QUIT and c or None
     # }}}
 
-_desc = lambda o: o.l[o.s][-1]=='+' and TreeMenu(o.w, o.l[o.s])
-_ascd = lambda o: TreeMenu(o.w, o.l[o.s]+'/../../', o.x['here'])
-_tree = lambda o: TreeMenu(o.w)
-
-class TreeMenu(Menu): # {{{
-    def _dots(o):
-        o.dots = not o.dots
-        o.l = o.mklist(o.x['here'])
+class DirtMenu(Menu):
+    _desc = lambda o: o.l[o.s][-1]=='+' and TreeMenu(o.w, o.l[o.s])
+    _ascd = lambda o: TreeMenu(o.w, o.l[o.s]+'/../../', o.x['here'])
+    _tree = lambda o: TreeMenu(o.w)
+    def _book(o):
+        p = twiddle(o.l[o.s])
+        if p not in BOOK: BOOK.append(p)
     def _save(o):
         p = twiddle(o.l[o.s])
         if p not in DIRT: DIRT.append(p)
     m = dict(Menu.m.items() + {
             C.KEY_RIGHT: _desc,
             C.KEY_LEFT:  _ascd,
-            ord('s'):    _save,
+            ord('b'):    _book,
             ord('d'):    _tree,
-            ord('e'):    lambda o: EnvMenu(o.w),
+            ord('s'):    _save,
             ord('h'):    lambda o: TreeMenu(o.w, '~'),
+            ord('~'):    lambda o: HomeMenu(o.w, o.x['here']),
+            }.items())
+
+class TreeMenu(DirtMenu): # {{{
+    def _dots(o):
+        o.dots = not o.dots
+        o.l = o.mklist(o.x['here'])
+    m = dict(DirtMenu.m.items() + {
+            ord('e'):    lambda o: EnvMenu(o.w),
             ord('.'):    _dots,
             }.items())
     def mklist(self, p):
@@ -122,23 +173,38 @@ class TreeMenu(Menu): # {{{
         super(TreeMenu, self).__init__(w, l, s, {'here': twiddle(p)})
     # }}}
 
-
-class EnvMenu(Menu): # {{{
+class EnvMenu(DirtMenu): # {{{
     def _del(o):
         DIRT.remove(twiddle(o.l[o.s]))
         Menu._del(o)
-    m = dict(Menu.m.items() + {
-            C.KEY_RIGHT: _desc,
-            C.KEY_LEFT:  _ascd,
-            ord('d'):    _tree,
+    m = dict(DirtMenu.m.items() + {
             ord('q'):    Menu._done,
             ord('x'):    _del,
             }.items())
-    def __init__(self, w):
+    def __init__(self, w, h=None):
         h = twiddle(cwd(), True)
         l = sorted([ twiddle(x, True) for x in DIRT ])
         s = (h in l and l.index(h) or len(l)/2)
         super(EnvMenu, self).__init__(w, l, s, {'here': h})
+    # }}}
+
+class HomeMenu(DirtMenu): # {{{
+    def __init__(self, w, h):
+        h = twiddle(cwd(), True)
+        l = sorted([ '~'+x for x in HOMES.keys() ])
+        s = (h in l and l.index(h) or len(l)/2)
+        super(HomeMenu, self).__init__(w, l, s, {'here': h})
+    # }}}
+
+class BookmarkMenu(DirtMenu): # {{{
+    def _del(o):
+        BOOK.remove(twiddle(o.l[o.s]))
+        Menu._del(o)
+    def __init__(self, w, h):
+        h = twiddle(cwd(), True)
+        l = sorted([ twiddle(x, True) for x in BOOK ])
+        s = (h in l and l.index(h) or len(l)/2)
+        super(HomeMenu, self).__init__(w, l, s, {'here': h})
     # }}}
 
 def wrap(f): # {{{
@@ -167,6 +233,7 @@ if __name__ == '__main__': # {{{
         while isinstance(m, Menu): m = m.run()
         return m.replace(' +','')
     p = untwid(wrap(run_menus))
-    if OLDD != DIRT:     print >>sys.stderr, 'DIRT=' + ':'.join(DIRT)
-    if p and p != cwd(): print >>sys.stderr, 'cd ' + p
+    if OLDD != DIRT:     print >>sys.stderr, 'DIRT=' + ':'.join(DIRT), ';',
+    if p and p != cwd(): print >>sys.stderr, 'cd ' + p, ';'
+    del BOOK
     # }}}
