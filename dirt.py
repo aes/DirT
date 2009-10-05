0668d2aa95d02708e890d2a147f32ad6b0f9ab0d
Anders Eurenius <anders.eurenius@snowmen.se>
2009-10-05 15:13:48 +0200

    Fix removing from session and bookmarks, Bookmarks now uses DirNames.

--- dirt.py
+++ dirt.py
@@ -114,8 +114,8 @@ class BookmarkFile(object): # {{{
     def __init__(self):
         try:    self.l = sorted([ DirName.fetch(l.strip())
                                   for l in file(expanduser('~/.dirt_bm')) ]
-                                or ['~'])
-        except: self.l = ['~']
+                                or [DirName.fetch('~')])
+        except: self.l = [DirName.fetch('~')]
         self.c = False
     def __del__(self):
         """Save if changed."""
@@ -238,6 +238,7 @@ class DirtMenu(Menu): # {{{
             ord('h'):    lambda o: TreeMenu(o.w, '~'),
             ord('q'):    Menu._done,
             ord('r'):    _subs,
+            ord('x'):    lambda o: o._del(),
             ord('~'):    lambda o: HomeMenu(o.w, o.x['here']),
             }.items())
     # }}}
@@ -295,7 +296,7 @@ class HomeMenu(DirtMenu): # {{{
 
 class BookmarkMenu(DirtMenu): # {{{
     def _del(o):
-        BOOK.remove(o.l[o.s].s)
+        BOOK.remove(o.l[o.s])
         Menu._del(o)
     def __init__(self, w, h=None):
         h = DirName.fetch(h or cwd())
